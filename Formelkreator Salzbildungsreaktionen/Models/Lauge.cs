39289adaf41315c558439a1c1dc271c1a6f0d0a8
Nick Deibus <nickdeibus@outlook.de>
2020-09-24 22:40:07 +0200

    Initial Project

---
+++ Formelkreator Salzbildungsreaktionen/Models/Lauge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formelkreator_Salzbildungsreaktionen.Models
+{
+    public class Lauge : Verbindung
+    {
+        public Lauge(string formelName) : base(formelName)
+        {
+
+        }
+    }
+}
---
+++ Formelkreator Salzbildungsreaktionen/Models/Verbindung.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formelkreator_Salzbildungsreaktionen.Models
+{
+    public class Verbindung
+    {
+        private string _FormelName;
+
+        public string FormelName
+        {
+            get { return _FormelName; }
+            set { _FormelName = value; }
+        }
+
+        public Verbindung(string formelName)
+        {
+            this.FormelName = formelName;
+        }
+    }
+}
